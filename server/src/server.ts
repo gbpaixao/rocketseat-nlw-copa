b3c092eb817c56f2eaa50eecb3e646082af2a5c7
gbpaixao <gustavobezerrapaixao@gmail.com>
2022-11-02 00:25:34 -0300

    feat: server initial commit

---
+++ server/src/server.ts
@@ -0,0 +1,25 @@
+import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client'
+import Fastify from 'fastify'
+
+const prisma = new PrismaClient({
+  log: ['query'],
+})
+
+async function bootstrap() {
+  const fastify = Fastify({
+    logger: true,
+  })
+
+  fastify.register(cors, {
+    origin: true,
+  })
+
+  fastify.get('/pools/count', async () => {
+    return { count: await prisma.pool.count() }
+  })
+
+  await fastify.listen({ port: 3333, host: '0.0.0.0' })
+}
+
+bootstrap()
